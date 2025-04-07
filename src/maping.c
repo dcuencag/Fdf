edd34077b1d2f78bdd315b3a520c1e8a314e92bf
Daniel Cuenca Gutierrez <dancuenc@c2r6s4.42madrid.com>
2025-04-07 14:29:25 +0200

    check perspective, something wrong

--- src/maping.c
+++ src/maping.c
@@ -10,22 +10,23 @@ int	**maping(int fd)
 	int	y;
 	double p_x;
 	int	p_y;
+	double zoom = 40;
 
 	x = 0;
 	p_x = 0;
 	p_y = 0;
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1200, 750, "Fil de Fer");
+	mlx_win = mlx_new_window(mlx, 3000, 2000, "Fil de Fer");
 	while ((together = get_next_line(fd)))
 	{
 		splited = ft_split(together, ' ');
 		y = 0;
 		while (splited[y])
 		{
-			p_x = ((y * cos(0.5236)) - (x * cos(0.5236)));
-			p_y = ft_atoi(splited[y]) - ((y * sin(0.5236)) - (x * sin(0.5236)));
+			p_x = ((y * zoom * cos(0.5236)) - (x * zoom * cos(0.5236)));
+			p_y = (ft_atoi(splited[y]) * (zoom / 2) - ((y * zoom * sin(0.5236)) - (x * zoom * sin(0.5236))));
 			printf("(%f,%d)\n", p_x, p_y);
-			mlx_pixel_put(mlx, mlx_win, p_y + 600, p_x + 375, 0x00FFA500);
+			mlx_pixel_put(mlx, mlx_win, p_x + 1150, p_y + 1000, 0x00FFA500);
 			y++;
 		}
 		free(together);
